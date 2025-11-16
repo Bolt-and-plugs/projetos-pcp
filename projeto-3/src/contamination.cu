ca8c02d75e69a6406a56878f929857fdea2ad5d1
LHLobato <luislobato35@gmail.com>
2025-11-16 15:01:47 -0300

    burro burro burro nao pronto burro

--- projeto-3/src/contamination.cu
+++ projeto-3/src/contamination.cu
@@ -2,11 +2,13 @@
 #include <cuda_runtime.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <curand.h>        
+#include <curand_kernel.h> 
 #include "utils.h" 
 
 #define ITERATION_END 404
 #define ITERATION_CONTINUE 101
+
 
 
 #define GET_STATE(R, C) \
@@ -55,7 +57,7 @@ void read_input(const char *path, int ***A, int *N, int *M) {
 int verify_end_population(int **A, int N, int M) {
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
-            if (A[i][j] == -1 || A[i][j] == -2) {
+            if (A[i][j] != 0) {
                 return ITERATION_CONTINUE;
             }
         }
@@ -64,8 +66,7 @@ int verify_end_population(int **A, int N, int M) {
 }
 
 
-void write_output(const char *output_path, int **A, int N, int M) {
-    long total_dead = 0; 
+void write_output(const char *output_path, int **A, int N, int M, int total_dead) {
     long total_survivors = 0; 
     
 
@@ -92,24 +93,17 @@ void write_output(const char *output_path, int **A, int N, int M) {
     
     fprintf(fp, "Total de Sobreviventes (Saudáveis ou Contaminados): %ld\n", total_survivors);
 
-    fprintf(fp, "Total de Pessoas Mortas na Matriz Final: %ld\n", total_dead); 
+    fprintf(fp, "Total de Pessoas Mortas na Matriz Final: %d\n", total_dead); 
     
     fclose(fp);
     printf("Resultados escritos em: %s\n", output_path);
 }
 
 
-__device__ int simple_lcg_rand(int index, unsigned int seed) {
-    unsigned long long state = (unsigned long long)index * 1103515245 + seed;
-    state = state * 1103515566 + 12345;
-    return (int)((state >> 16) % 10000);
-}
-
-
-__global__ void execute_iter(int *A_current, int *A_next, int N, int M, unsigned int seed) {
+__global__ void execute_iter(int *A_current, int *A_next, int N, int M, curandState_t *states, int *total_dead) {
     int total_elements = N * M;
     int index = threadIdx.x + blockIdx.x * blockDim.x;
-
+    
     if (index >= total_elements) return;
 
     int r = index / M; 
@@ -131,8 +125,11 @@ __global__ void execute_iter(int *A_current, int *A_next, int N, int M, unsigned
         }
     }
     else if (current_state == -1) {
-        int x = simple_lcg_rand(index, seed);
-
+        int element_id = r * M + c;   
+        curandState_t local = states[element_id];
+        unsigned int random_val = curand(&local);
+        states[element_id] = local;
+        int x = random_val % 10000;
         if (x <= 999) { // 0.1 
             next_state = 1; 
         } else if (x <= 3999) { // 0.3
@@ -141,17 +138,22 @@ __global__ void execute_iter(int *A_current, int *A_next, int N, int M, unsigned
             next_state = -2; 
         }
     }
-    else if (current_state == -2) { 
+    else if (current_state == -2) {
+        atomicAdd(total_dead, 1);
         next_state = 0; 
     }
 
     A_next[index] = next_state;
 }
 
+__global__ void setup_kernel(curandState_t *state, unsigned long seed, int N_total) {
+        int id = threadIdx.x + blockIdx.x * blockDim.x;
+        if (id < N_total) {
+            curand_init(seed, id, 0, &state[id]);
+        }
+    }
 
 int main(int argc, char **argv){
-    
-    srand(time(NULL)); 
     if(argc < 5){
         puts("Invalid Number of arguments: <file_path> <num_threads> <num_blocks> <device>");
         return -1;
@@ -169,9 +171,10 @@ int main(int argc, char **argv){
     int max_iter = N*M;
     int size = N*M*sizeof(int);
 
-    int *h_current_flat = (int*)malloc(size); 
+    int *h_current_flat = (int*)malloc(size); //matriz interpretada como vetor
     int *h_next_flat = (int*)malloc(size); 
-    
+    int total_dead = 0;
+    int *d_total_dead;
     if (h_current_flat == NULL || h_next_flat == NULL) {
         fprintf(stderr, "Host memory allocation failed\n");
         return -1;
@@ -179,7 +182,7 @@ int main(int argc, char **argv){
 
     for (int r = 0; r < N; r++) {
         for (int c = 0; c < M; c++) {
-            h_current_flat[r * M + c] = space[r][c];
+            h_current_flat[r * M + c] = space[r][c]; //atribuindo valores para a matriz vetorizada
         }
     }
 
@@ -187,20 +190,34 @@ int main(int argc, char **argv){
     int *d_next = NULL;    
 
     if (cudaMalloc((void **)&d_current, size) != cudaSuccess || 
-        cudaMalloc((void **)&d_next, size) != cudaSuccess) {
+        cudaMalloc((void **)&d_next, size) != cudaSuccess || cudaMalloc((void **)&d_total_dead, (int)sizeof(int))!= cudaSuccess) {
         printf("Error at allocating memory on GPU\n");
         return -1;
     }
-    
-    cudaMemcpy(d_current, h_current_flat, size, cudaMemcpyHostToDevice);
-    
+
+    cudaMemset(d_total_dead, 0, sizeof(int));
+    cudaMemcpy(d_current, h_current_flat, size, cudaMemcpyHostToDevice); //copiando o conteúdo para a GPU
+    curandState_t *d_states;
+    size_t state_size = N*M * sizeof(curandState_t);
+
     int i = 0;
     unsigned int seed;
+    if (cudaMalloc((void **)&d_states, state_size) != cudaSuccess) {
+        printf("Error at allocating cuRAND states on GPU\n");
+        return -1;
+    }
+
+
     
+    unsigned long initial_seed = 42; 
+    
+    int blocks_setup = (N*M + num_threads - 1) / num_threads;
+    
+    setup_kernel<<<blocks_setup, num_threads>>>(d_states, initial_seed, N*M);
+
     for(i = 0; i < max_iter; i++){
-        seed = (unsigned int)rand(); 
         
-        execute_iter<<<num_blocks, num_threads>>>(d_current, d_next, N, M, seed);
+        execute_iter<<<num_blocks, num_threads>>>(d_current, d_next, N, M, d_states, d_total_dead);
         
         cudaError_t err = cudaGetLastError();
         if (err != cudaSuccess) {
@@ -212,7 +229,7 @@ int main(int argc, char **argv){
         cudaDeviceSynchronize(); 
         
 
-        cudaMemcpy(h_next_flat, d_next, size, cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_next_flat, d_next, size, cudaMemcpyDeviceToHost); //Copiando a matriz de iteração atual para o Host
         
 
         for (int r = 0; r < N; r++) {
@@ -220,13 +237,13 @@ int main(int argc, char **argv){
                 space[r][c] = h_next_flat[r * M + c];
             }
         }
-        
-        if(verify_end_population(space, N, M) == ITERATION_END) {
+        cudaMemcpy(&total_dead, d_total_dead, sizeof(int), cudaMemcpyDeviceToHost); //Copiando a matriz de iteração atual para o Host
+        if(verify_end_population(space, N, M) == ITERATION_END) { //verificação se acabou ou não as iterações
             printf("Simulation ended after %d iterations due to population stability.\n", i + 1);
             break; 
         }
 
-        int *temp = d_current;
+        int *temp = d_current; //atualização para iteração atual na GPU. 
         d_current = d_next;
         d_next = temp; 
     }
@@ -236,7 +253,7 @@ int main(int argc, char **argv){
     }
 
 
-    write_output(output_file, space, N, M);
+    write_output(output_file, space, N, M, total_dead);
     
 
     for (int r = 0; r < N; r++) free(space[r]);
@@ -247,6 +264,7 @@ int main(int argc, char **argv){
 
     cudaFree(d_current);
     cudaFree(d_next);
-
+    cudaFree(d_states);
+    cudaFree(d_total_dead);
     return 0;
 }
