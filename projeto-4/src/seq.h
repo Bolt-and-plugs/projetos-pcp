39ca0c3e8d2106aab15be7674165616f84b10a71
Carlos Eduardo Nogueira Silva <cen.silva@unesp.br>
2025-11-18 21:45:49 -0300

    sequencial

--- projeto-4/src/seq.h
+++ projeto-4/src/seq.h
@@ -5,6 +5,21 @@
 #include "utils.h"
 #include "math.h"
 #include "time.h"
+#include "stdlib.h"
+
+typedef enum _status { 
+  dead_twice = -3, 
+  dead = -2, 
+  infected = -1, 
+  empty = 0, 
+  healthy = 1 
+} status;
+
+bool handle_args(int argc, char **argv);
+bool is_every_elem_one(int **A, int N, int M);
+bool is_every_elem_zero(int **A, int N, int M);
+void seq_process(int **A, int N, int M);
+void *seq_worker(int **A, int N, int M);
 
 int main(int argc, char **argv);
 
